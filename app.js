360d6d1f132e82b1c6fb4a2cd4ade6321b0b8929
Chidananda Singh <chidru11@gmail.com>
2025-05-10 15:23:10 +0530

    Check point -2

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
-let boxes = document.querySelectorAll(".box")
-let reset = document.querySelector("#reset-button")
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset-button");
 
 let turn0 = false;
 
 const winPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
-]
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
-boxes.forEach((box) =>{
-    box.addEventListener("click",()=>{
-        console.log("Box was clicked");
-        if(turn0){
-            box.innerText = "0";
-            turn0= false;
-        }
-        else{
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turn0) {
+      box.innerText = "0";
+      turn0 = false;
+    } else {
+      box.innerText = "X";
+      turn0 = true;
+    }
+    box.disabled = true;
 
-            box.innerText = "X";
-            turn0= true;
-        }
-        box.disabled = true;
-    });
+    checkWinner();
+  });
 });
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("Winner ",pos1Val);
+      }
+    }
+  }
+};
